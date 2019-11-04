c7f6fe2413631993c7e84fd58daa058593668979
talentedunicorn <emangalu@gmail.com>
2019-11-04 12:31:42 +0800

    Added middleware requireValidToken and added protected profile path

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,18 @@
 
 const routes = require("express").Router();
 const auth = require("./auth");
+const { requireValidToken } = require("./middleware");
 
 routes.get("/", (req, res) => {
   res.status(200).json({ data: "Pinged Auth Service..." });
 });
 
 routes.post("/auth", auth);
+
+routes.all("/profile", requireValidToken);
+
+routes.get("/profile", (req, res) => {
+  res.send({ data: "Profile data" });
+});
 
 module.exports = routes;
